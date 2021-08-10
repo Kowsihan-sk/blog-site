86f28e67d4bc3a4691447427781be100353b8aa0
Kowsihan <kowsihan2sk@gmail.com>
2021-08-10 18:15:25 +0530

    deployment ready

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv"
 dotenv.config();
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 import connectDB from "./config/db.js";
 import router from "./routes/route.js";
@@ -13,6 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/", router);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import axios from "axios";
 dotenv.config();
 
-const URL = `http://localhost:5000`;
+const URL = ``;
 
 export const createPost = async (post) => {
     try {
