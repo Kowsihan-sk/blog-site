3f6c0b2a1ece15c1fbf19a3979f6e5e10c4cc50b
Kowsihan <kowsihan2sk@gmail.com>
2021-08-10 20:43:31 +0530

    deployment 2.0

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,6 @@ app.use(cors());
 
 app.use("/", router);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (req, res) => {
